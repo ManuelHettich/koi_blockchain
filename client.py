a55fd7a712db05ebc2e75bbbfad0243f99c25fc7
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2021-07-25 21:35:43 +0200

    Use argparse to parse CLI arguments for client.py

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import pickle
 import requests
@@ -5,57 +6,57 @@ import block
 
 
 def main():
-    if len(sys.argv) != 3:
-        print(f"usage: {sys.argv[0]} host port")
-        sys.exit()
-    else:
-        # Read in the hostname / address and port of the server from the command line arguments
-        host = sys.argv[1]
-        port = sys.argv[2]
+    # Read in the hostname / address and port of the server from the command line arguments
+    # with argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", help="hostname or ip address of the server, e.g. 127.0.0.1")
+    parser.add_argument("port", help="port of the server, e.g. 8000", type=int)
+    args = parser.parse_args()
+    host = args.host
+    port = args.port
 
-        print("[send] / [check] a local file or [quit]")
-        while True:
-            # Ask for an input from user
-            user_input = input("> ").split()
+    # Ask for user input what to do next
+    print("[send] / [check] a local file or [quit]")
+    while True:
+        # Ask for an input from user
+        user_input = input("> ").split()
 
-            if len(user_input) < 1 or len(user_input) > 2:
-                print("[send] / [check] a local file or [quit]")
-                continue
+        if len(user_input) < 1 or len(user_input) > 2:
+            print("[send] / [check] a local file or [quit]")
+            continue
 
-            # Parse command and filepath from provided user input
-            command = user_input[0]
-            filepath = user_input[1]
+        # Parse command and filepath from provided user input
+        command = user_input[0]
+        # Quit program if the user entered "quit"
+        if command == "quit":
+            sys.exit()
+        filepath = user_input[1]
+        if command == "send":
+            # Send a new file to the blockchain server
 
-            if command == "quit":
-                # Quit program
-                sys.exit()
+            # Generate the necessary blocks of the local file
+            blocks = block.generate_blocks(filepath)
+            # Collect all blocks into a single binary file using pickle
+            blocks_pickled = pickle.dumps(blocks)
+            # Send the collected blocks in a single transfer to the server
+            response = requests.post(f"http://{host}:{port}/send",
+                                     files={"file": blocks_pickled})
+            # Print the response from the server
+            print(f"Response from Server: {response.text}")
 
-            elif command == "send":
-                # Send a new file to the blockchain server
+        elif command == "check":
+            # Check if a local file is stored on the blockchain server by sending its
+            # SHA256 hash (checksum)
+            file_hash = block.calculate_file_hash(filepath)
+            response = requests.get(f"http://{host}:{port}/check",
+                                    params={"file_hash": file_hash})
 
-                # Generate the necessary blocks of the local file
-                blocks = block.generate_blocks(filepath)
-                # Collect all blocks into a single binary file using pickle
-                blocks_pickled = pickle.dumps(blocks)
-                # Send the collected blocks in a single transfer to the server
-                response = requests.post(f"http://{host}:{port}/send",
-                                         files={"file": blocks_pickled})
-                # Print the response from the server
-                print(f"Response from Server: {response.text}")
+            # Print the response from the server
+            print(f"Response from Server: {response.text}")
 
-            elif command == "check":
-                # Check if a local file is stored on the blockchain server by sending its
-                # SHA256 hash (checksum)
-                file_hash = block.calculate_file_hash(filepath)
-                response = requests.get(f"http://{host}:{port}/check",
-                                        params={"file_hash": file_hash})
-
-                # Print the response from the server
-                print(f"Response from Server: {response.text}")
-
-            else:
-                print("The provided command is unknown")
-                print("[send] / [check] a local file or [quit]")
+        else:
+            print("The provided command is unknown")
+            print("[send] / [check] a local file or [quit]")
 
 if __name__ == "__main__":
     main()
